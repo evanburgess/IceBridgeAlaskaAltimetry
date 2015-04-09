5965aca899c92053b83464892901f82acdbdaabf
evanburgess <evanburgess@gmail.com>
2015-04-09 14:08:59 -0800

    cleaning figures 2,3,S2,S3,S6

--- Figures/Figure_2.py
+++ Figures/Figure_2.py
@@ -1,44 +1,11 @@
 # -*- coding: utf-8 -*-
-import xlrd
-import re
-import unicodedata
 import numpy as N
-import datetime as dtm
-import os
-import glob
-import simplekml as kml
-import subprocess
 import matplotlib.pyplot as plt
-from osgeo import gdal
-from types import *
-import sys
-import time
-from mpl_toolkits.basemap import Basemap
-from matplotlib.collections import PatchCollection
-from osgeo.gdalnumeric import *
-import matplotlib.path as mpath
-import matplotlib.patches as mpatches
-import matplotlib.mlab as mlab
-import scipy.misc
-import matplotlib
-import matplotlib.colors as colors
-import matplotlib.cm as cmx
-import scipy.stats as stats
-import scipy.stats.mstats as mstats
-from numpy.ma import MaskedArray, masked, nomask
-import numpy.ma as ma
+import matplotlib as mpl
 import pickle
-import datetime as dtm
-
-import ConfigParser
-
-cfg = ConfigParser.ConfigParser()
-cfg.read(os.path.dirname(__file__)+'/setup.cfg')
-sys.path.append(re.sub('[/][^/]+$','',os.path.dirname(__file__)))
-
 from Altimetry.Interface import *
     
-
+#CREATES THE LITTLE INTERVAL LENGTH INSET PLOT
 def make_interval_length_plot(axes,interval_list,colorlist,label=None):
 
     plt.rc("font", **{"sans-serif": ["Arial"],"size": 10})
@@ -69,26 +36,8 @@ def make_interval_length_plot(axes,interval_list,colorlist,label=None):
     axes.get_xaxis().tick_bottom()
     axes.set_ylim([0,3])
     
-    
-    
-plot_hist=False
-
-user_where=["FLOOR((ergi.glactype::real-9000)/100)=0 AND gltype.surge='f'","FLOOR((ergi.glactype::real-9000)/100)=1 AND glnames.name!='Columbia'","FLOOR((ergi.glactype::real-9000)/100)=2 AND glnames.name!='Bering' AND glnames.name!='YakutatEast' AND glnames.name!='YakutatWest' AND gltype.surge='f'","gltype.surge='f'"]
-#user_where=["FLOOR((ergi.glactype::real-9000)/100)=0 AND gltype.surge='f'","FLOOR((ergi.glactype::real-9000)/100)=1 AND glnames.name!='Columbia'","FLOOR((ergi.glactype::real-9000)/100)=2 AND glnames.name!='Bering' AND glnames.name!='YakutatWest' AND glnames.name!='YakutatEast' AND gltype.surge='f'"]
-outroot = ["Tidewater","Lake","Land","All"]
-color = ['k','g','r','r']
-color = N.array([[0,0,0],[163,100,57],[45,80,150],[75,150,38]])/255.
-min_intervals=N.array([0,3,5,10,3,5])
-intervalsmax=N.array([3,5,10,30,30,30])
-labels = ['<=3yr intervals','3-5 yrs','5-10yrs','>=10yrs','>=3yrs','>=5yrs','Before 2003','After 2002']
-shpfiles = ['intervals_less3yr','intervals_3_5yrs','intervals5_10yrs','intervals_more10yrs','intervals_more3yrs','intervals_more5yrs','Before2003','After2002']
-div1 = '2000-01-01'
-i=0
-
-startfromscratch=True
-
-
-
+#IT TAKES A WHILE TO QUERY THE DATABASE SO THIS DOES THE QUERIES, SAVES THE RESULTS FOR EASY DEVELOPING OF THE PLOT ITSELF
+startfromscratch=False
 if startfromscratch:    
 
     surveyeddata = GetLambData(verbose=False,longest_interval=True,interval_max=30,interval_min=5,by_column=True,as_object=True,results=False)  # 
@@ -98,7 +47,7 @@ if startfromscratch:
     surveyeddata.calc_dz_stats()
     #surveyeddata.extend_upper_extrap()
 
-    land = GetLambData(verbose=False,longest_interval=True,interval_max=30,interval_min=5,by_column=True,as_object=True, userwhere="FLOOR((ergi.glactype::real-9000)/100)=0 AND gltype.surge='f'",results=False) 
+    land = GetLambData(verbose=False,longest_interval=True,interval_max=30,interval_min=5,by_column=True,as_object=True, userwhere="FLOOR((ergi.glactype::real-9000)/100)=0 AND gltype.surge='f'",results=False)   # zzz
     land.convert085()
     land.fix_terminus()
     land.remove_upper_extrap(remove_bottom=False)
@@ -107,7 +56,7 @@ if startfromscratch:
     land.extend_upper_extrap()
     
 
-    tide = GetLambData(verbose=False,longest_interval=True,interval_max=30,interval_min=5,by_column=True,as_object=True, userwhere="FLOOR((ergi.glactype::real-9000)/100)=1 AND glnames.name!='Columbia'",results=False) 
+    tide = GetLambData(verbose=False,longest_interval=True,interval_max=30,interval_min=5,by_column=True,as_object=True, userwhere="FLOOR((ergi.glactype::real-9000)/100)=1 AND glnames.name!='Columbia'",results=False)   # zzz
     tide.convert085()
     tide.fix_terminus()
     tide.remove_upper_extrap(remove_bottom=False)
@@ -115,7 +64,7 @@ if startfromscratch:
     tide.calc_dz_stats(too_few=4)
     tide.extend_upper_extrap()
 
-    lake = GetLambData(verbose=False,longest_interval=True,interval_max=30,interval_min=5,by_column=True,as_object=True, userwhere="FLOOR((ergi.glactype::real-9000)/100)=2 AND glnames.name!='Bering' AND glnames.name!='YakutatEast' AND glnames.name!='YakutatWest' AND gltype.surge='f'",results=False) 
+    lake = GetLambData(verbose=False,longest_interval=True,interval_max=30,interval_min=5,by_column=True,as_object=True, userwhere="FLOOR((ergi.glactype::real-9000)/100)=2 AND glnames.name!='Bering' AND glnames.name!='YakutatEast' AND glnames.name!='YakutatWest' AND gltype.surge='f'",results=False)  # zzz 
     lake.convert085()
     lake.fix_terminus()
     lake.remove_upper_extrap(remove_bottom=False)
@@ -123,120 +72,109 @@ if startfromscratch:
     lake.calc_dz_stats(too_few=4)
     lake.extend_upper_extrap()
 
-    columbia = GetLambData(verbose=False,longest_interval=True,interval_min=5,by_column=True,as_object=True, userwhere="glnames.name='Columbia'") 
+    columbia = GetLambData(verbose=False,longest_interval=True,interval_min=5,by_column=True,as_object=True, userwhere="glnames.name='Columbia'")   # zzz
     columbia.convert085()
     columbia.fix_terminus()
     columbia.remove_upper_extrap(remove_bottom=False)
     columbia.normalize_elevation()
     
-    hubbard = GetLambData(verbose=False,longest_interval=True,interval_min=5,by_column=True,as_object=True, userwhere="glnames.name='Hubbard'") 
+    hubbard = GetLambData(verbose=False,longest_interval=True,interval_min=5,by_column=True,as_object=True, userwhere="glnames.name='Hubbard'")   # zzz
     hubbard.convert085()
     hubbard.fix_terminus()
     hubbard.remove_upper_extrap(remove_bottom=False)
     hubbard.normalize_elevation()
     
-    wolverine = GetLambData(verbose=False,longest_interval=True,interval_min=5,by_column=True,as_object=True, userwhere="glnames.name='Wolverine'") 
+    wolverine = GetLambData(verbose=False,longest_interval=True,interval_min=5,by_column=True,as_object=True, userwhere="glnames.name='Wolverine'")   # zzz
     wolverine.convert085()
     wolverine.fix_terminus()
     wolverine.remove_upper_extrap(remove_bottom=False)
     wolverine.normalize_elevation()
     
-    gulkana = GetLambData(verbose=False,longest_interval=True,interval_min=5,by_column=True,as_object=True, userwhere="glnames.name='Gulkana'") 
+    gulkana = GetLambData(verbose=False,longest_interval=True,interval_min=5,by_column=True,as_object=True, userwhere="glnames.name='Gulkana'")   # zzz
     gulkana.convert085()
     gulkana.fix_terminus()
     gulkana.remove_upper_extrap(remove_bottom=False)
     gulkana.normalize_elevation()
     
-    pickle.dump([surveyeddata,land,tide,lake,columbia,hubbard,wolverine,gulkana], open( "/Users/igswahwsmcevan/Desktop/temp4.p", "wb" ))
+    yakeast = GetLambData(verbose=False,longest_interval=True,interval_min=5,by_column=True,as_object=True, userwhere="glnames.name='YakutatEast'")   # zzz
+    yakeast.convert085()
+    yakeast.fix_terminus()
+    yakeast.remove_upper_extrap(remove_bottom=False)
+    yakeast.normalize_elevation()
+    
+    yakwest = GetLambData(verbose=False,longest_interval=True,interval_min=5,by_column=True,as_object=True, userwhere="glnames.name='YakutatWest'")   # zzz
+    yakwest.convert085()
+    yakwest.fix_terminus()
+    yakwest.remove_upper_extrap(remove_bottom=False)
+    yakwest.normalize_elevation()
+    
+    pickle.dump([surveyeddata,land,tide,lake,columbia,hubbard,wolverine,gulkana,yakeast,yakwest], open( "/Users/igswahwsmcevan/Desktop/temp4.p", "wb" ))  # zzz
 else:
-    surveyeddata,land,tide,lake,columbia,hubbard,wolverine,gulkana = pickle.load(open( "/Users/igswahwsmcevan/Desktop/temp4.p", "rb" ))
+    surveyeddata,land,tide,lake,columbia,hubbard,wolverine,gulkana,yakeast,yakwest = pickle.load(open( "/Users/igswahwsmcevan/Desktop/temp4.p", "rb" ))   # zzz
 
-#GLACIERS THAT ARE THICKENING AT THE TERMINUS AND THINNING UP HIGH
-switch = N.logical_and(N.array([N.mean(dz[:20]) for dz in tide.normdz])>0,N.array([N.mean(dz[40:80]) for dz in tide.normdz])<0)
 
-yakeast = GetLambData(verbose=False,longest_interval=True,interval_min=5,by_column=True,as_object=True, userwhere="glnames.name='YakutatEast'") 
-yakeast.fix_terminus()
-yakeast.remove_upper_extrap(remove_bottom=False)
-yakeast.normalize_elevation()
 
-yakwest = GetLambData(verbose=False,longest_interval=True,interval_min=5,by_column=True,as_object=True, userwhere="glnames.name='YakutatWest'") 
-yakwest.fix_terminus()
-yakwest.remove_upper_extrap(remove_bottom=False)
-yakwest.normalize_elevation()
-
-samples_lim=None
-err_lim=None
-title=None
+#PLOTTING VARIABLES
 alphafill = 0.3
+color = N.array([[0,0,0],[163,100,57],[45,80,150],[75,150,38]])/255.
 
+#SETTING UP FIGURE STRUCTURE
 fig = plt.figure(figsize=[4,8])
 ax = fig.add_subplot(3,1,1)
 ax2 = fig.add_subplot(3,1,2)
 ax3 = fig.add_subplot(3,1,3)
 fig.subplots_adjust(bottom=0.05,top=0.99,right=0.96,hspace=0.12)
-plt.rc("font", **{"sans-serif": ["Arial"],"size": 13})
-#ax.set_title('Surface Elevation Change: %s Glaciers' % outroot[i])
 
-#ax.set_xlabel('Normalized Elevation', fontsize=11)
+#FONTS
+plt.rc("font", **{"sans-serif": ["Arial"],"size": 13})
+
+#PLOTTING LAND IN TOP PLOT
+ax.plot(land.norme,land.dzs_mean,'-',color=N.array([163,100,57])/255.,linewidth=2.5,label='Land')
+ax.plot(land.norme,land.dzs_mean-land.dzs_sem,'-',land.norme,land.dzs_mean+land.dzs_sem,'-',color=N.array([163,100,57])/255.,linewidth=1)
+ax.fill_between(land.norme,land.dzs_mean+land.dzs_std,land.dzs_mean-land.dzs_std,alpha=alphafill,color= N.array([163,100,57])/255.,lw=0.01)
+for dz in land.normdz:ax.plot(land.norme,dz,'-k',linewidth=0.4,alpha=0.2)
+
+ax.plot(gulkana.norme,gulkana.normdz[0],'--k',linewidth=1.,alpha=0.6)
+ax.plot(wolverine.norme,wolverine.normdz[0],'-.k',linewidth=1.,alpha=0.6)
+
+#SETTING AXES LIMITS AND FONT SIZES
 ax.set_ylim([-6,1])
 ax.set_xlim([0,1])
 for tick in ax.xaxis.get_major_ticks():tick.label.set_fontsize(10) 
 for tick in ax.yaxis.get_major_ticks():tick.label.set_fontsize(10) 
 
-ax.plot(land.norme,land.dzs_mean,'-',color=N.array([163,100,57])/255.,linewidth=2.5,label='Land')
-ax.plot(land.norme,land.dzs_mean-land.dzs_sem,'-',land.norme,land.dzs_mean+land.dzs_sem,'-',color=N.array([163,100,57])/255.,linewidth=1)
-ax.fill_between(land.norme,land.dzs_mean+land.dzs_std,land.dzs_mean-land.dzs_std,alpha=alphafill,color= N.array([163,100,57])/255.,lw=0.01)
-
+#PLOTTING LAKE IN MIDDLE PLOT
 ax2.plot(lake.norme,lake.dzs_mean,'-',color='green',linewidth=2.5,label='Lake')
 ax2.plot(lake.norme,lake.dzs_mean-lake.dzs_sem,'-',lake.norme,lake.dzs_mean+lake.dzs_sem,'-',color='green',linewidth=1)
 ax2.fill_between(lake.norme,lake.dzs_mean+lake.dzs_std,lake.dzs_mean-lake.dzs_std,alpha=alphafill,color= 'green',lw=0.01)
-
-ax2.set_ylim([-6,1])
-ax2.set_xlim([0,1])
-
-font = matplotlib.font_manager.FontProperties(family='Arial', weight='bold', size=15)
-ax.annotate('A',[0.07,0.5],horizontalalignment='center',verticalalignment='center',fontproperties=font)
-ax2.annotate('B',[0.07,0.5],horizontalalignment='center',verticalalignment='center',fontproperties=font)
-ax2.set_ylabel("Elevation Change (m w. eq. yr"+"$\mathregular{^{-1})}$", fontsize=11,labelpad=3)
-
-#ax.legend(fontsize=9,loc='lower right', bbox_to_anchor=(0.956, 0.47))
-for dz in land.normdz:ax.plot(land.norme,dz,'-k',linewidth=0.4,alpha=0.2)
 for dz in lake.normdz:ax2.plot(lake.norme,dz,'-k',linewidth=0.4,alpha=0.2)
 
-#ax3 = fig.add_axes([0.15,0.07,0.8,0.43])
-plt.rc("font", **{"sans-serif": ["Arial"],"size": 13})
+ax2.plot(yakeast.norme,yakeast.normdz[0],'--k',linewidth=1.,alpha=0.6)
 
-#ax3.set_ylabel("Elevation Change (m yr"+"$\mathregular{^{-1})}$", fontsize=11,labelpad=-2)
+#SETTING AXES LIMITS AND FONT SIZES LABELS ETC
+ax2.set_ylim([-6,1])
+ax2.set_xlim([0,1])
+ax2.set_ylabel("Elevation Change (m w. eq. yr"+"$\mathregular{^{-1})}$", fontsize=11,labelpad=3)
+
+#PLOTTING TIDEWATER IN BOTTOM PLOT
+ax3.plot(tide.norme,tide.dzs_mean,'-',color='blue',linewidth=2,label='Tidewater')
+ax3.plot(tide.norme,tide.dzs_mean-tide.dzs_sem,'-',tide.norme,tide.dzs_mean+tide.dzs_sem,'-',color='blue',linewidth=0.7)
+ax3.fill_between(tide.norme,tide.dzs_mean+tide.dzs_std,tide.dzs_mean-tide.dzs_std,alpha=alphafill,color= 'blue',lw=0.01)
+ax3.fill_between(ax.get_xlim(),N.repeat(N.array(ax.get_ylim()[0]),2),N.repeat(N.array(ax.get_ylim()[1]),2),alpha=0.1,color= 'black',lw=0.01,zorder=0)
+for dz in tide.normdz:ax3.plot(tide.norme,dz,'-k',linewidth=0.4,alpha=0.2)
+ax3.plot(tide.norme,dz,'-k',linewidth=0.3,alpha=0.3)
+    
+ax3.plot(columbia.norme,columbia.normdz[0],'--k',linewidth=1.,alpha=0.6)
+ax3.plot(hubbard.norme,hubbard.normdz[0],'-.k',linewidth=1.,alpha=0.6)
+
+#SETTING AXES LIMITS AND FONT SIZES LABELS ETC
 ax3.set_xlabel('Normalized Elevation', fontsize=11)
 ax3.set_ylim([-10,12])
 ax3.set_xlim([0,1])
 for tick in ax3.xaxis.get_major_ticks():tick.label.set_fontsize(10) 
 for tick in ax3.yaxis.get_major_ticks():tick.label.set_fontsize(10) 
 
-ax3.plot(tide.norme,tide.dzs_mean,'-',color='blue',linewidth=2,label='Tidewater')
-ax3.plot(tide.norme,tide.dzs_mean-tide.dzs_sem,'-',tide.norme,tide.dzs_mean+tide.dzs_sem,'-',color='blue',linewidth=0.7)
-ax3.fill_between(tide.norme,tide.dzs_mean+tide.dzs_std,tide.dzs_mean-tide.dzs_std,alpha=alphafill,color= 'blue',lw=0.01)
-ax3.fill_between(ax.get_xlim(),N.repeat(N.array(ax.get_ylim()[0]),2),N.repeat(N.array(ax.get_ylim()[1]),2),alpha=0.1,color= 'black',lw=0.01,zorder=0)
-
-for j,dz in enumerate(tide.normdz):
-    if switch[j]: ax3.plot(tide.norme,dz,'-k',linewidth=0.3,alpha=0.3)   # can make these a separate color
-    else: ax3.plot(tide.norme,dz,'-k',linewidth=0.3,alpha=0.3)
-    
-ax3.plot(columbia.norme,columbia.normdz[0],'--k',linewidth=1.,alpha=0.6)
-ax3.plot(hubbard.norme,hubbard.normdz[0],'-.k',linewidth=1.,alpha=0.6)
-
-ax.plot(gulkana.norme,gulkana.normdz[0],'--k',linewidth=1.,alpha=0.6)
-ax.plot(wolverine.norme,wolverine.normdz[0],'-.k',linewidth=1.,alpha=0.6)
-
-ax2.plot(yakeast.norme,yakeast.normdz[0],'--k',linewidth=1.,alpha=0.6)
-#ax2.plot(yakwest.norme,yakwest.normdz[0],'-.k',linewidth=1.,alpha=0.6)
-
-ax3.annotate('C',[0.07,10],horizontalalignment='center',verticalalignment='center',fontproperties=font)
-
-#ax.legend(fontsize=9,loc='upper right', bbox_to_anchor=(0.945, 0.6))
-#ax2.legend(fontsize=9,loc='upper right', bbox_to_anchor=(0.945, 0.6))
-#ax3.legend(fontsize=9,loc='upper right', bbox_to_anchor=(0.57, 0.99))
-
+#INSERTING INSET BOXPLOT FIGURES
 ax4 = fig.add_axes([0.627,0.755,0.3,0.07])
 make_interval_length_plot(ax4,[land.interval/365.],color[[1]],label="Land")
 
@@ -246,23 +184,12 @@ make_interval_length_plot(ax5,[lake.interval/365.],color[[3]],label='Lake')
 ax6 = fig.add_axes([0.627,0.259,0.3,0.07])
 make_interval_length_plot(ax6,[tide.interval/365.],['blue'],label="Tidewater")
 
+#ADDING A,B,C LABELS
+font = mpl.font_manager.FontProperties(family='Arial', weight='bold', size=15)
+ax.annotate('A',[0.07,0.5],horizontalalignment='center',verticalalignment='center',fontproperties=font)
+ax2.annotate('B',[0.07,0.5],horizontalalignment='center',verticalalignment='center',fontproperties=font)
+ax3.annotate('C',[0.07,10],horizontalalignment='center',verticalalignment='center',fontproperties=font)
 
-#ax3 = ax3.twiny()
-#ax3.set_xlim([0,15])
-#plt.text(0.1,14,"Calving Flux (Gt a"+"$^{-1}$"+")", fontsize=11)
-#ax3.set_xticks([1, 2, 3,4,5])
-#for tick in ax3.xaxis.get_major_ticks():tick.label2.set_fontsize(10) 
-#
-#
-##PLOTTING FLUXES AS CIRCLES
-#termidz = [dz.compressed()[0] for dz in tide.normdz]
-#
-#for i,flx in enumerate(tide.eb_bm_flx.astype(float)):
-#    circle1=plt.Circle([0,termidz[i]],flx,color='r',zorder=-1)
-#    ell = mpatches.Ellipse(xy=[0,termidz[i]], width=flx*2, height=0.3, angle=0)
-#    ax3.add_artist(ell)
-#    ell.set_alpha(0.7)
-#    ell.set_color('r')
 plt.show()
 
 fig.savefig("/Users/igswahwsmcevan/Papers/AK_altimetry/Figures/dhdtprofiles4_1.jpg",dpi=500)
--- Figures/Figure_S2_and_Figure_S6.py
+++ Figures/Figure_S2_and_Figure_S6.py
@@ -1,92 +1,46 @@
-import psycopg2
-import xlrd
-import re
-import unicodedata
 import numpy as N
-from time import mktime
-from datetime import datetime
-from time import mktime
-import time
-import os
-import glob
-import simplekml as kml
-import subprocess
 import datetime as dtm
-from types import *
-import sys
-import ppygis
-import StringIO
-import shapefile
-from osgeo import osr
-from pylab import *
 import matplotlib as plt
-import pickle
-import datetime as dtm
-import matplotlib.dates as mdates
-import ConfigParser
-from glob import glob
-
-cfg = ConfigParser.ConfigParser()
-cfg.read(os.path.dirname(__file__)+'/setup.cfg')
-sys.path.append(re.sub('[/][^/]+$','',os.path.dirname(__file__)))
-
 from Altimetry.Interface import *
 
+#DETERMINES IF A DATE IS WITHIN AN INTERVAL
 def datebtw(date, daterange):return date>daterange[0] and date<daterange[1]
 
-def hyp_med(bins,count):
-    cum = count.cumsum()
-    
-    idx = N.abs(cum-(cum[-1]/2)).argmin()
-    m,b = N.polyfit(bins[idx-3:idx+3],(cum-(cum[-1]/2))[idx-3:idx+3],1)
-    
-    return -b/m
-d = GetLambData(verbose=False,longest_interval=True,interval_max=30,interval_min=5,by_column=True,as_object=True)
+#QUERYING DATABASE
+d = GetLambData(verbose=False,longest_interval=True,interval_max=30,interval_min=5,by_column=True,as_object=True) #ZZZ
 
-
+#GETTING MIDDATE OF INTERVALS
 td = [d2-d1 for d1,d2 in zip(d.date1,d.date2)]
 middate = [date+dtm.timedelta(days=dtime.days/2) for dtime,date in zip(td,d.date1)]
 
+#MIDDATE YEARS
 year = N.array([md.year for md in middate])
+
+#GROUPING THEM INTO EVEN NUMBERED YEARS
 biyear = N.where(year%2==0,year-1,year)
 
-#year = biyear
+#BUILDING A HISTOGRAM
 freq = N.bincount(year)
 x = N.where(freq!=0)[0]
-datex = [datetime.date(yr,1,1) for yr in x]
-
-
-
+datex = [dtm.date(yr,1,1) for yr in x]
 
 #FINDING DISTRIBUTION OF SAMPLES THROUGH TIME
 data = GetLambData(longest_interval=True,as_object=True, orderby="ergi.region,ergi.name",interval_min=5)
-timeline = [datetime.date(t,1,1) for t in N.arange(1993,2015)]
+timeline = [dtm.date(t,1,1) for t in N.arange(1993,2015)]
 count = N.zeros(len(timeline))
 
 for j,t in enumerate(timeline):
     for i in xrange(len(data.date2)):
         if datebtw(t,[data.date1[i],data.date2[i]]):count[j]+=1
 ordtime = [i.toordinal() for i in timeline]        
-print datetime.date.fromordinal(hyp_med(ordtime,count).astype(int))
 
-
-
-
-
-
-
-
-
-
-
-
-
+#FIGURE S2 SETUP
 fig = plt.figure(figsize=[3.5,3.5])
 ax = fig.add_subplot(111)
 plt.rc("font", **{"sans-serif": ["Arial"],"size": 12})
-
 fig.subplots_adjust(left=0.20,bottom=0.2, right=0.95, top=0.95,wspace=0.35)
 
+#PLOTTING 
 bar = ax.bar(datex,freq[x],width=356,color='0.5')
 ax.plot_date(timeline,count,'-',color='k',lw=2,zorder=0.1)
 #ax.xaxis_date()
@@ -100,30 +54,28 @@ ax.set_ylabel('Count')
 fig.savefig("/Users/igswahwsmcevan/Papers/AK_altimetry/Figures/sampling_distribution_temporal.jpg",dpi=300)
 plt.show()
 
-
-
+#########################################################################################################
+#FIGURE S6 SETUP
 fig2 = plt.figure(figsize=[3.5,3.5])
 ax2 = fig2.add_subplot(111)
 plt.rc("font", **{"sans-serif": ["Arial"],"size": 12})
-
 fig2.subplots_adjust(left=0.20,bottom=0.2, right=0.93, top=0.95,wspace=0.35)
 
+#QUERYING DATABASE FOR HYPOSMETRY
+surv = GetSqlData2("SELECT bins::real,SUM(area)::real/1000000. as area FROM resultsauto WHERE surveyed = 't' GROUP BY bins ORDER BY bins;")#zzz
+unsu = GetSqlData2("SELECT bins::real,SUM(area)::real/1000000. as area FROM resultsauto GROUP BY bins ORDER BY bins;")#zzz
 
-
-surv = GetSqlData2("SELECT bins::real,SUM(area)::real/1000000. as area FROM resultsauto WHERE surveyed = 't' GROUP BY bins ORDER BY bins;")
-unsu = GetSqlData2("SELECT bins::real,SUM(area)::real/1000000. as area FROM resultsauto GROUP BY bins ORDER BY bins;")
-
+#PLOTTING
 ax2.bar(unsu['bins'],unsu['area'],width=30,color=[0.6,0.6,1],zorder=0,linewidth=0,label="Alaska Region")
 ax2.bar(surv['bins'],surv['area'],width=30,color='b',linewidth=0,label="Surveyed Glaciers")
+
+#AXES LABEL,LIMITS, TICKS
 ax2.set_ylabel('Area (km'+"$\mathregular{^{2})}$")
 ax2.set_xlabel("Elevation (m)")
 ax2.set_xticks(N.arange(0,6001,2000))
 ax2.set_yticks(N.arange(0,1800,500))
 ax2.set_xlim([0,6000])
 ax2.legend(fontsize=10)
-
-#ax.annotate("A",xy=[0,1],xycoords='axes fraction',xytext = [8,-8],textcoords='offset points',weight='bold',size=13,va='top',ha='left')
-#ax2.annotate("B",xy=[0,1],xycoords='axes fraction',xytext = [8,-8],textcoords='offset points',weight='bold',size=13,va='top',ha='left')
 
 fig2.savefig("/Users/igswahwsmcevan/Papers/AK_altimetry/Figures/sampling_distribution_spatial.jpg",dpi=300)
 plt.show()
